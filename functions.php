2e5b509d75219e1093df530273bf09a589e34637
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2022-11-26 21:30:01 +0100

    action hook before products added

--- functions.php
+++ functions.php
@@ -82,19 +82,20 @@ add_action('wp_enqueue_scripts', 'custom_main_js');
 
 // Hook ,need to find out why it doesnt work
 
-// function add_section_before_products()
-// {
-//     echo '<section class="section-before-products">';
-//     echo '<div class="container">';
-//     echo '<div class="row">';
-//     echo '<div class="col-md-12">';
-//     echo '<h2>Our Products</h2>';
-//     echo '</div>';
-//     echo '</div>';
-//     echo '</div>';
-//     echo '</section>';
-// }
-// add_action('storefront_before_content', 'add_section_before_products', 10);
+function add_section_before_products()
+{
+    echo '<section class="section-before-products">';
+    echo '<div class="container">';
+    echo '<div class="row">';
+    echo '<div class="col-md-12">';
+    echo '<h2>Our Products</h2>';
+    echo '</div>';
+    echo '</div>';
+    echo '</div>';
+    echo '</section>';
+
+} 
+    add_action('woocommerce_before_shop_loop', 'add_section_before_products', 10);
 
 function remove_storefront_sidebar()
 {
