9c0c6350635a997e6593da101178f9d2c39dc33d
KennieA92 <91551710+KennieA92@users.noreply.github.com>
2022-11-28 10:02:29 +0100

    Tweaks to homepage

--- page-home.php
+++ page-home.php
@@ -1,6 +1,6 @@
 <?php get_header() ?>
 <section id="carousel-section">
-    <div class="carousel slide carousel-fade" data-bs-ride="carousel">
+    <div id="event-carousel" class="carousel slide carousel-fade" data-bs-ride="carousel">
         <div class="carousel-inner d-flex">
             <?php
             $args = array('post_type' => 'product', 'posts_per_page' => -1, 'product_cat' => 'new_product', 'orderby' => 'rand');
@@ -8,22 +8,22 @@
             while ($loop->have_posts()) : $loop->the_post();
                 global $product; ?>
                 <div style="background: <?php echo the_field("beer_color") ?>" class="carousel-item <?php if ($loop->current_post == 0) echo 'active'; ?> d-flex justify-content-center align-items-center">
-                    <a href="<?php echo get_permalink() ?>" class="d-block col-12 col-md-4 ">
-                        <img src="<?php echo get_the_post_thumbnail_url() ?>" class="d-block col-12 slide-image" alt="...">
+                    <a href="<?php echo get_permalink() ?>" class="d-block col-12 col-md-6 ">
+                        <img src="<?php echo get_field("event_image")["url"] ?>" class="d-block col-12 slide-image" alt="...">
                     </a>
-                    <div class="carousel-caption">
+                    <!-- <div class="carousel-caption">
                         <h5><?php the_title() ?></h5>
                         <p><?php echo $product->get_price_html(); ?></p>
-                    </div>
+                    </div> -->
                 </div>
             <?php endwhile; ?>
             <?php wp_reset_query(); ?>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+        <button class="carousel-control-prev" type="button" data-bs-target="#event-carousel" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
         </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+        <button class="carousel-control-next" type="button" data-bs-target="#event-carousel" data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
         </button>
